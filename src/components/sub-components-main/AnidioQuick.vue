9398b7947b9ca4d3c7132dcf4a083e47a87107dd
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-20 13:06:16 +0200

    feat: add ourworks text container and elements

--- src/components/sub-components-main/AnidioQuick.vue
+++ src/components/sub-components-main/AnidioQuick.vue
@@ -46,7 +46,6 @@ export default {
 
 .anidio-quick {
   width: 100%;
-  height: 500px;
   border: 2px solid yellow;
   //background elements settings
   background-image: url(../../assets/shape-bottom.png),
--- src/components/sub-components-main/OurWorks.vue
+++ src/components/sub-components-main/OurWorks.vue
@@ -15,7 +15,63 @@ export default {
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <div class="our-works">
+    <!--* text container -->
+    <div class="text-container">
+      <span class="slogan">Our Works</span>
+      <h1 class="title">Featured Productions</h1>
+      <span class="spanning"
+        >Here's just a small sample of some of those projects that we're quite
+        proid of. <br />
+        If you're looking for something specific feel free to get in contact
+        with us.</span
+      >
+    </div>
+    <!--* images cont-->
+    <div class="images-cont"></div>
+    <div class="button-cont"></div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Bagel+Fat+One&family=Share+Tech+Mono&display=swap");
+@import "../../partials/variables";
+
+//general css
+.our-works {
+  width: 100%;
+  height: 500px;
+  border: 2px dashed rgb(0, 255, 153);
+}
+// text container css
+.text-container {
+  width: 80%;
+  margin: 30px auto;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: center;
+  border: 1px dashed orange;
+  .slogan {
+    font-size: 15px;
+    color: $azure_color;
+    margin: 5px 0;
+  }
+  .title {
+    color: $purple_color;
+    font-size: 50px;
+    font-family: "Bagel Fat One", cursive;
+    font-family: "Share Tech Mono", monospace;
+    margin: 10px 0;
+  }
+  .spanning {
+    font-size: 15px;
+    color: $graypurple_color;
+    margin: 10px 0;
+    line-height: 25px;
+  }
+}
+
+//images css
+</style>
