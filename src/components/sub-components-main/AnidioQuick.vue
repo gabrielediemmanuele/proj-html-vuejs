a511cb1a3ca23e869aea8f6227bdda46a2e37852
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-20 12:38:40 +0200

    feat: add an object for generate counters slot

--- src/components/sub-components-main/AnidioQuick.vue
+++ src/components/sub-components-main/AnidioQuick.vue
@@ -7,7 +7,17 @@ export default {
 </script>
 
 <template>
-  <div class="anidio-quick"></div>
+  <div class="anidio-quick">
+    <div class="facts-cont">
+      <div class="title-cont"><h1>Anidio Quick Facts</h1></div>
+      <div class="counters-cont">
+        <div class="counter" v-for="counter in counters">
+          <span>{{ counter.totalValue }}</span>
+          <span>{{ counter.description }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -26,5 +36,30 @@ export default {
   background-repeat: no-repeat, no-repeat, no-repeat, no-repeat;
   background-position: bottom, top, center, center;
   background-size: auto, auto, contain, cover;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  //facts cont
+  .facts-cont {
+    width: 80%;
+    height: 300px;
+    border: 2px dashed lime;
+
+    .title-cont h1 {
+      font-family: "Bagel Fat One", cursive;
+      font-family: "Share Tech Mono", monospace;
+      color: $white_color;
+      border: 1px dashed orange;
+      text-align: center;
+      padding: 20px 0;
+      font-size: 40px;
+    }
+    .counters-cont {
+      width: 100%;
+      border: 1px dashed yellow;
+    }
+  }
 }
 </style>
