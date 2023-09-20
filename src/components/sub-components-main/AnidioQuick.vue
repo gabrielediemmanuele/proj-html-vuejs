0c6e03c5a8348a3792c9c3e5799b80a0c95b6c46
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-20 12:24:58 +0200

    feat: add Anidio quick bg elements and settings

--- src/components/sub-components-main/AnidioQuick.vue
+++ src/components/sub-components-main/AnidioQuick.vue
@@ -1,21 +1,30 @@
 <script>
-// import MyComponent from "./components/MyComponent.vue";
-
 export default {
   data() {
-    return {
-      title: "Hello world",
-    };
+    return {};
   },
-
-  // components: {
-  //   MyComponent,
-  // },
 };
 </script>
 
 <template>
-  <h1>{{ title }}</h1>
+  <div class="anidio-quick"></div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+//font and variables scss
+@import url("https://fonts.googleapis.com/css2?family=Bagel+Fat+One&family=Share+Tech+Mono&display=swap");
+@import "../../partials/variables";
+
+.anidio-quick {
+  width: 100%;
+  height: 500px;
+  border: 2px solid yellow;
+  //background elements settings
+  background-image: url(../../assets/shape-bottom.png),
+    url(../../assets/shape-top.png), url(../../assets/globe.png),
+    url(../../assets/counter-bg.png);
+  background-repeat: no-repeat, no-repeat, no-repeat, no-repeat;
+  background-position: bottom, top, center, center;
+  background-size: auto, auto, contain, cover;
+}
+</style>
