c90b08d6d20b68c661d53d0200bfaa7289317d92
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-20 12:07:16 +0200

    feat: add cards with different gradients system.

--- src/components/sub-components-main/LoveAnimation.vue
+++ src/components/sub-components-main/LoveAnimation.vue
@@ -7,16 +7,22 @@ export default {
           cardImage: "../../../public/img/smart1.png",
           cardTitle: "3D modeling",
           text: "We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep. We'll drill deep to deliver above and beyond your expectations",
+          bgGradient:
+            "linear-gradient( 3deg, rgb(255, 228, 208) 0%, rgba(255, 255, 255))",
         },
         {
           cardImage: "../../../public/img/smart2.png",
           cardTitle: "Collaboration",
           text: "We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep. We'll drill deep to deliver above and beyond your expectations",
+          bgGradient:
+            "linear-gradient( 3deg, rgb(238, 235, 252, 1) 0%, rgba(255, 255, 255, 1))",
         },
         {
           cardImage: "../../../public/img/smart3.png",
           cardTitle: "Creativity",
           text: "We constantly ask questions. It helps us shape your story and deliver the results you crave. Yep. We'll drill deep to deliver above and beyond your expectations",
+          bgGradient:
+            "linear-gradient( 3deg, rgb(253,232,234,1) 0%, rgba(255,255,255,1))",
         },
       ],
     };
@@ -38,9 +44,14 @@ export default {
 
   <!--* CARD CONTAINER-->
   <div class="cards-cont">
-    <div class="card" v-for="card in cards">
+    <div
+      class="card"
+      v-for="(card, index) in cards"
+      :key="index"
+      :style="{ 'background-image': card.bgGradient }"
+    >
       <img :src="card.cardImage" :alt="card.cardTitle" />
-      <h4>{{ card.cardTitle }}</h4>
+      <h3>{{ card.cardTitle }}</h3>
       <p>{{ card.text }}</p>
     </div>
   </div>
@@ -93,19 +104,29 @@ export default {
 
 .card {
   width: calc(80% / 3);
-  height: 310px;
-  padding: 10px;
+  padding: 15px;
   border: 2px dashed red;
 
   display: flex;
   flex-direction: column;
   align-items: center;
+  border-radius: 20px;
 
   img {
     width: 50px;
+    margin-bottom: 15px;
   }
 
   h3 {
+    margin-bottom: 15px;
+  }
+
+  p {
+    font-size: 14px;
+    text-align: center;
+    margin-bottom: 15px;
+    line-height: 25px;
+    font-weight: lighter;
   }
 }
 </style>
