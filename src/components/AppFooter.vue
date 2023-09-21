a39e051846f255762760b0e8b90a5e3366d4ec8c
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-09-21 10:32:15 +0200

    feat: footer ended

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -32,7 +32,10 @@ export default {
       </div>
       <!--* Copyright and Socials Icons -->
       <div class="bottom-cont">
-        <span></span>
+        <span
+          >Copyright &copy; 2023 <a href="#">Softvius.</a> All rights reserved.
+          Made by C0deWhisper.</span
+        >
         <div class="icons-cont">
           <div class="single-icon">
             <font-awesome-icon icon="fa-brands fa-facebook" />
@@ -55,7 +58,9 @@ export default {
 .footer-cont {
   width: 100%;
   border: 2px dashed lime;
+  padding-top: 40px;
 }
+//NAV CONTAINER CONTENTS
 .nav-cont {
   width: 80%;
   margin: 0 auto;
@@ -68,12 +73,13 @@ export default {
     list-style-type: none;
     display: flex;
     align-items: center;
-    margin: 0 30px 0 auto;
+    margin: 0;
     padding: 0;
 
     li {
-      margin: 0 10px 0 10px;
+      margin: 0 5px 0 5px;
       color: black;
+      font-size: 12px;
       //hover
       &:hover {
         margin: 0 10px 0 10px;
@@ -83,26 +89,46 @@ export default {
   }
 }
 
-.icons-cont {
+//COPYRIGHT AND ICONS
+.bottom-cont {
+  width: 80%;
+  margin: 0 auto;
+  padding: 10px;
   display: flex;
-  justify-content: center;
   align-items: center;
-  z-index: 1;
-  margin-top: 30px;
-
-  .single-icon {
-    width: 45px;
-    height: 45px;
-    margin: 0 10px;
-    color: $purple_color;
-    font-size: 20px;
-    border: 1px solid $purple_color;
-    background-color: $white_color;
-    padding: 10px;
-    border-radius: 50%;
+  justify-content: space-between;
+  border: 1px dashed orange;
+  //social icons
+  .icons-cont {
     display: flex;
-    align-items: center;
     justify-content: center;
+    align-items: center;
+    z-index: 1;
+
+    .single-icon {
+      width: 45px;
+      height: 45px;
+      margin: 0 5px;
+      color: $purple_color;
+      font-size: 20px;
+      border: 1px solid $purple_color;
+      background-color: $white_color;
+      padding: 10px;
+      border-radius: 50%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+
+  //span
+  span {
+    font-size: 12px;
+    font-weight: 500;
+
+    a {
+      text-decoration: none;
+    }
   }
 }
 </style>
